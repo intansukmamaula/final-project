54478b90055ba586d67fab2246d2c5b19d84828f
Intan Sukma Maula <intansukma647@gmail.com>
2023-05-23 16:01:26 +0700

    mengerjakan final

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Intan Sukma Maula';
+
+const studentId = 'FE5589449'
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
